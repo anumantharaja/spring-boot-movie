2173273033c616c718656eea3a011014b01a8389
anumantharaja <anumantharaja@gmail.com>
2018-03-26 10:15:17 +0530

    Update RedisConfig.java

--- src/main/java/com/michaelcgood/config/RedisConfig.java
+++ src/main/java/com/michaelcgood/config/RedisConfig.java
@@ -10,12 +10,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-	    @Bean
-	        public <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory connectionFactory) {
-			        RedisTemplate<String, T> template = new RedisTemplate<>();
-				        template.setConnectionFactory(connectionFactory);
-					        template.setKeySerializer(new StringRedisSerializer());
-						        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-							        return template;
-								    }
+@Bean
+public <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory connectionFactory) {
+RedisTemplate<String, T> template = new RedisTemplate<>();
+template.setConnectionFactory(connectionFactory);
+template.setKeySerializer(new StringRedisSerializer());
+template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+return template;
+}
 }
